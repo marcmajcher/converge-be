0773068b426da36135c3caa911da758e8fac6e33
Marc Majcher <majcher@gmail.com>
2020-03-02 16:42:10 -0600

    login

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', function(req, res, next) {
+router.get('/', function(req, res) {
   res.render('index', { title: 'My App' });
+});
+
+/* smoke test route */
+router.get('/booyah', (req, res) => {
+  res.send('booyah');
 });
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const { OAuth2Client } = require('google-auth-library');
+const jwt = require('jsonwebtoken');
+
 const APP_ID =
   '730966416306-dv0e9pb4m0k6khl2nrn5r9vskv2j8hmk.apps.googleusercontent.com';
 const client = new OAuth2Client(APP_ID);
+const SECRET = '__C_H_A_N_G_E_M_E__';
+
+const userInfo = {};
 
 router.post('/', function(req, res, next) {
   client
@@ -12,8 +17,27 @@ router.post('/', function(req, res, next) {
       audience: APP_ID,
     })
     .then(ticket => ticket.getPayload()['sub'])
-    .then(id => res.send(`USER ID: ${id}`))
+    .then(user_id => {
+      userInfo[user_id] = req.body.userData;
+      res.json({ token: jwt.sign(userInfo[user_id], SECRET) });
+    })
     .catch(console.error);
+});
+
+router.get('/logout', (req, res) => {
+  req.session = null;
+  res.redirect('/');
+});
+
+router.post('/verify', (req, res) => {
+  jwt.verify(req.body.token, SECRET, (err, data) => {
+    if (err) {
+      console.error(err);
+      res.sendStatus(500);
+    } else {
+      res.json(userInfo[data.googleId]);
+    }
+  });
 });
 
 module.exports = router;

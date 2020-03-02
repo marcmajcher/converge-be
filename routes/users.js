ce4e413cc1e99d9979236a2a82393bf747c5cd19
Marc Majcher <majcher@gmail.com>
2020-03-02 13:47:05 -0600

    login

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,19 @@
 const express = require('express');
 const router = express.Router();
+const { OAuth2Client } = require('google-auth-library');
+const APP_ID =
+  '730966416306-dv0e9pb4m0k6khl2nrn5r9vskv2j8hmk.apps.googleusercontent.com';
+const client = new OAuth2Client(APP_ID);
 
 router.post('/', function(req, res, next) {
-  console.log(req.body)
-  res.send('respond with a resource');
+  client
+    .verifyIdToken({
+      idToken: req.body.token,
+      audience: APP_ID,
+    })
+    .then(ticket => ticket.getPayload()['sub'])
+    .then(id => res.send(`USER ID: ${id}`))
+    .catch(console.error);
 });
 
 module.exports = router;

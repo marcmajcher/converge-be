b3cc5fcdd9345c42e477021d256cd285318fc2c0
Marc Majcher <majcher@gmail.com>
2020-04-01 20:04:10 -0500

    gameid

--- socket.js
+++ socket.js
@@ -1,6 +1,7 @@
 const socketioJwt = require('socketio-jwt');
 const socketio = require('socket.io');
 const numberStation = require('./numberStation');
+const randomWords = require('random-words');
 const secret = '__C_H_A_N_G_E_M_E__';
 
 module.exports = function socketInit(server) {
@@ -16,16 +17,30 @@ module.exports = function socketInit(server) {
 
   io.on('connection', socket => {
     const googleId = socket.decoded_token;
-    console.log(`Client ${googleId} connected`);
+    console.log(`>> Client ${googleId} connected`);
 
     socket.on('booyah', data => console.log(`BOOYAH: ${data}`));
-    socket.on('startNewGame', () => console.log('ASDAT START NEW GAME'));
-    
+    socket.on('startNewGame', onStartNewGame);
+
     socket.on('disconnect', () => onDisconnect(googleId));
-    socket.emit('info', 'Connected to socket - Welcome to Converge!')
+    socket.emit('info', 'Connected to socket - Welcome to Converge!');
   });
 };
 
 function onDisconnect(id) {
-  console.log(`User ${id} disconnected.`);
+  console.log(`<< User ${id} disconnected.`);
+}
+
+function onStartNewGame() {
+  const gameId = randomWords({
+    exactly: 2,
+    join: '-',
+    maxLength: 7,
+  });
+  this.emit('info', `Starting a new game! ID: ${gameId}`);
+  this.emit('newGameCreated', { gameId, socketId: this.id });
+  this.join(gameId);
+
+  console.log('-----');
+  console.log(this.adapter.rooms[gameId]);
 }

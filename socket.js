2921c75a690344d3f8d544c8f7d7806634dbac75
Marc Majcher <majcher@gmail.com>
2020-04-01 19:06:55 -0500

    routing

--- socket.js
+++ socket.js
@@ -18,13 +18,14 @@ module.exports = function socketInit(server) {
     const googleId = socket.decoded_token;
     console.log(`Client ${googleId} connected`);
 
-    socket.on('disconnect', 
-    () => onDisconnect(googleId));
+    socket.on('booyah', data => console.log(`BOOYAH: ${data}`));
+    socket.on('startNewGame', () => console.log('ASDAT START NEW GAME'));
+    
+    socket.on('disconnect', () => onDisconnect(googleId));
+    socket.emit('info', 'Connected to socket - Welcome to Converge!')
   });
 };
 
 function onDisconnect(id) {
   console.log(`User ${id} disconnected.`);
 }
-
-
